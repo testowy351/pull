1c8fa36df1d8676e380a1b1fcd5b33dd0db26efd
h3xds1nz <gamingshard@protonmail.com>
2024-10-03 14:44:53 +0200

    Remove allocations on Tokenizer for generated structs

--- src/Microsoft.DotNet.Wpf/src/WindowsBase/System/Windows/Generated/Size.cs
+++ src/Microsoft.DotNet.Wpf/src/WindowsBase/System/Windows/Generated/Size.cs
@@ -172,7 +172,7 @@ namespace System.Windows
 
             Size value;
 
-            String firstToken = th.NextTokenRequired();
+            ReadOnlySpan<char> firstToken = th.NextTokenRequiredAsSpan();
 
             // The token will already have had whitespace trimmed so we can do a
             // simple string compare.
@@ -182,9 +182,8 @@ namespace System.Windows
             }
             else
             {
-                value = new Size(
-                    Convert.ToDouble(firstToken, formatProvider),
-                    Convert.ToDouble(th.NextTokenRequired(), formatProvider));
+                value = new Size(double.Parse(firstToken, formatProvider),
+                    double.Parse(th.NextTokenRequiredAsSpan(), formatProvider));
             }
 
             // There should be no more tokens in this string.

9c09ce2884d6dcbd8ddc97a202b8d3e9f3594682
h3xds1nz <gamingshard@protonmail.com>
2024-10-10 11:09:00 +0200

    Enable nullable support

--- src/Microsoft.DotNet.Wpf/src/System.Xaml/System/Xaml/MS/Impl/AssemblyNamespacePair.cs
+++ src/Microsoft.DotNet.Wpf/src/System.Xaml/System/Xaml/MS/Impl/AssemblyNamespacePair.cs
@@ -2,8 +2,6 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
-#nullable disable
-
 using System.Diagnostics;
 using System.Reflection;
 
@@ -21,9 +19,9 @@ namespace System.Xaml.MS.Impl
             _clrNamespace = clrNamespace;
         }
 
-        public Assembly Assembly
+        public Assembly? Assembly
         {
-            get { return (Assembly)_assembly.Target; }
+            get { return (Assembly?)_assembly.Target; }
         }
 
         public string ClrNamespace
